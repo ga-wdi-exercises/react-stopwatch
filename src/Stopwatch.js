bd75c3ea32b1c2e2dd92e06c95cea639c9355c1b
Josue Rosales <jhosue.rosales@gmail.com>
2017-09-06 13:52:44 -0400

    first commit

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,55 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+
+  constructor(){
+    super()
+
+    this.state = {
+      time: 0,
+      paused: false
+    }
+
+
+    this.handleClickStart = this.handleClickStart.bind(this)
+    this.handleClickReset = this.handleClickReset.bind(this)
+    this.handleClickPaused = this.handleClickPaused.bind(this)
+  }
+
+
+  handleClickStart(){
+    if(!this.state.paused){
+      var x = setInterval(()=>{
+        this.setState({time: this.state.time + 1})
+      },1000)
+    }
+  }
+
+  handleClickReset(){
+    this.setState({time: 0, paused: true})
+  }
+
+  handleClickPaused(){
+    if(this.state.paused)
+    {
+      this.setState({paused: false})
+    }
+    else {
+      this.setState({paused: true})
+    }
+  }
+
+
   render() {
+
+
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.time}</h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.handleClickReset}>Reset</button>
+          <button onClick={this.handleClickStart}>Start</button>
+          <button onClick={this.handleClickPause}>Pause</button>
         </div>
       </div>
     );
