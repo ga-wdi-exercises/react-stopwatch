c9a1f405f12497b2d5c1e7b976252339f9bbc7f3
Spenser Holstein <spenser.holstein@gmail.com>
2017-10-03 20:04:28 -0400

    Stopwatch working, reset working, pause NOT working

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,51 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
-  render() {
+
+  constructor () {
+    super()
+    this.state = {
+      time: 0
+    }
+    this.startTimer = this.startTimer.bind(this)
+    this.setTimeCount = this.setTimeCount.bind(this)
+    this.resetTimeCount = this.resetTimeCount.bind(this)
+    this.pauseTimer = this.pauseTimer.bind(this)
+
+  }
+
+
+  setTimeCount () {
+    this.setState(prevState => ({time: prevState.time + 1}) )
+    console.log("Time Count Started.")
+    console.log(this.state.time)
+
+    setTimeout(this.setTimeCount, 500)
+  }
+
+  startTimer() {
+    setTimeout(this.setTimeCount, 500)
+  }
+
+  resetTimeCount () {
+    this.setState( {time: 0} )
+  }
+
+  pauseTimer () {
+    console.log("Paused Button Pushed")
+    // clearTimeout(setTimeCount)
+  }
+
+
+  render () {
+
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1> {this.state.time} </h1>
         <div className="controls">
-          <button>Reset</button>
-          <button>Start</button>
-          <button>Pause</button>
+          <button onClick={this.resetTimeCount}>Reset</button>
+          <button onClick={() => this.startTimer()}>Start</button>
+          <button onClick={() => this.pauseTimer()}>Pause</button>
         </div>
       </div>
     );
