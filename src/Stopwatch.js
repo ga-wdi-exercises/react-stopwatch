66f36751c58089f98bf2427731653e46d253944a
Aaliyah Melton <aaliyahmariah09@gmail.com>
2017-10-05 06:30:19 -0400

    Aaliyah's Stopwatch

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -2,14 +2,52 @@ import React, { Component } from 'react';
 import "./Stopwatch.css"
 
 class Stopwatch extends Component {
+  constructor(){
+    super()
+    this.state = {
+      seconds: 0,
+      paused: true
+    }
+    this.incrementer = null;
+  }
+  handleStart(e){
+    this.incrementer = setInterval(()=>
+      this.setState({
+        seconds: this.state.seconds + 1,
+        paused: false
+      })
+    ,1000)
+  }
+  handlePause(e){
+    let paused = !this.state.paused
+    if(paused){
+      clearInterval(this.incrementer)
+      this.setState({
+        paused,
+      })
+    }
+    else{
+      this.handleStart()
+    }
+  }
+  handleReset(e){
+    clearInterval(this.incrementer)
+    this.setState({
+      seconds: 0,
+      paused: true
+    })
+  }
   render() {
     return (
       <div className="stopwatch">
-        <h1>0</h1>
+        <h1>{this.state.seconds}</h1>
         <div className="controls">
           <button>Reset</button>
           <button>Start</button>
           <button>Pause</button>
+          <button onClick={e => this.handleReset(e)}>Reset</button>
+          <button onClick={e => this.handleStart(e)}>Start</button>
+          <button onClick={e => this.handlePause(e)}>Pause</button>
         </div>
       </div>
     );
