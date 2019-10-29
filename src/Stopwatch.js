85859dea1263b5cb1a02365914b43c6498e13136
thechiragarora <chirag.arora@dentsuaegis.com>
2019-10-29 20:52:48 +0530

    fix: format

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import "./Stopwatch.css"
+import React, { Component } from "react";
+import "./Stopwatch.css";
 
 class Stopwatch extends Component {
   constructor(props) {
@@ -8,57 +8,61 @@ class Stopwatch extends Component {
       time: {
         hours: 0,
         minutes: 0,
-        seconds: 0,
-      }, 
-      pause: true,
-    }
+        seconds: 0
+      },
+      pause: true
+    };
   }
 
-handleStart = () => {
-  this.interval = setInterval(
-    this.handleTime, 1000,
-  );
-}
+  handleStart = () => {
+    this.interval = setInterval(this.handleTime, 1000);
+  };
 
-handleReset = () => {
-  clearInterval(this.interval);
-  this.setState({ time: { seconds: 0, minutes: 0, hours: 0 }, pause: true});
-}
-
-handlePause = () => {
-  const { pause } = this.state;
-  if (pause) {
-    this.handleStart();
-  } else {
+  handleReset = () => {
     clearInterval(this.interval);
-    this.setState({ pause: true });
-  }
-}
+    this.setState({ time: { seconds: 0, minutes: 0, hours: 0 }, pause: true });
+  };
+
+  handlePause = () => {
+    const { pause } = this.state;
+    if (pause) {
+      this.handleStart();
+    } else {
+      clearInterval(this.interval);
+      this.setState({ pause: true });
+    }
+  };
 
   handleTime = () => {
-    let { time: {seconds, minutes, hours} } = this.state;
+    let {
+      time: { seconds, minutes, hours }
+    } = this.state;
 
     seconds = ++seconds;
-    if (seconds === 60 ) {
+    if (seconds === 60) {
       seconds = 0;
       minutes = ++minutes;
-    } else if (minutes === 60 ) {
+    } else if (minutes === 60) {
       seconds = 0;
       minutes = 0;
       hours = ++hours;
     }
 
     this.setState({ time: { seconds, minutes, hours }, pause: false });
-  }
+  };
 
   getFormattedTime = () => {
-    const { time: { hours, minutes, seconds }} = this.state;
-    return `${this.formatNumber(hours)}:${this.formatNumber(minutes)}:${this.formatNumber(seconds)}`
-  }
+    const {
+      time: { hours, minutes, seconds }
+    } = this.state;
+    return `${this.formatNumber(hours)}:${this.formatNumber(
+      minutes
+    )}:${this.formatNumber(seconds)}`;
+  };
 
-  formatNumber = (number) => {
-   return number < 10 ? '0' + number : number;
-  }
+  formatNumber = number => {
+    return number < 10 ? "0" + number : number;
+  };
 
   render() {
     return (
